c820e142173ab086813387230a4006c32cdfded3
Thomas Maurice <thomas@maurice.fr>
2025-01-10 14:24:49 +0100

    First commit

---
+++ lua/configs/conform.lua
@@ -0,0 +1,18 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+    go = { "gofumpt", "goimports-reviser", "golines"},
+    hcl = { "hcl" },
+    json = { "jq" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 1000,
+    lsp_fallback = true,
+  },
+}
+
+return options
