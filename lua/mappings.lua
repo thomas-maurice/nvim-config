8e86261a33bac1b9c8381513c40cd538685d99d6
Thomas Maurice <thomas@maurice.fr>
2025-01-11 16:04:42 +0100

    feat(mappings): Add sensible mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,32 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Telescope bindings
+local telescope = require "telescope.builtin"
+
+map("n", "<leader>ff", telescope.find_files, {})
+map("n", "<leader>fg", telescope.live_grep, {})
+map("n", "<leader>fb", telescope.buffers, {})
+map("n", "<leader>fh", telescope.help_tags, {})
+
+-- Tree
+map("n", "<leader>tf", ":NvimTreeFocus<CR>", {})
+
+-- Focus on a given buffer
+for i = 1, 9, 1 do
+  map("n", "<leader>t" .. i, i .. "gt", {})
+end
+
+for i = 1, 9, 1 do
+  map("n", "<leader>" .. i, function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
+
+-- Save and quit keymaps
+map("n", "<leader>w", ":w<CR>", {})
+map("n", "<leader>a", ":wa<CR>", {})
