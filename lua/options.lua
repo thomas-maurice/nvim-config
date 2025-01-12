b1b18d3d37756e8c74af131ae7e5d83d341c6137
Thomas Maurice <thomas@maurice.fr>
2025-01-12 09:59:51 +0100

    few vim options

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,8 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
-
+-- enable cursorline
+o.cursorlineopt = "both"
+-- term colours
+o.termguicolors = true
