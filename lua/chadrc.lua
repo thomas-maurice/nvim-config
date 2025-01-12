cdb8bf4cc2e5c0258d3dad00618bfd1d961be984
Thomas Maurice <thomas@maurice.fr>
2025-01-12 13:32:40 +0100

    color

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -23,7 +23,7 @@ M.ui = {
     style = "atom_colored", -- default/flat_light/flat_dark/atom/atom_colored
     abbr_maxwidth = 60,
     format_colors = {
-      tailwind = false, -- will work for css lsp too
+      tailwind = true, -- will work for css lsp too
       icon = "󱓻",
     },
   },
