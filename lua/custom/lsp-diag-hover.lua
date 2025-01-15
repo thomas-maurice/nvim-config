3e8b534668b380c4b973d14d823d4b9739b8b210
Thomas Maurice <thomas@maurice.fr>
2025-01-15 08:48:33 +0100

    feat(lsp): Hover notifications form LSP

--- lua/custom/lsp-diag-hover.lua
+++ lua/custom/lsp-diag-hover.lua
@@ -1,3 +1,22 @@
+---comment Returns the diagnostics at the cursor's position
+---@return string
+local function get_diag_at_cursor()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local winnr = vim.api.nvim_get_current_win()
+  local curx, cury = unpack(vim.api.nvim_win_get_cursor(winnr))
+  local diags = vim.diagnostic.get(bufnr, {
+    lnum = curx - 1,
+  })
+
+  local text = ""
+
+  for _, diag in pairs(diags) do
+    text = text .. diag.message .. "[" .. diag.source .. "]" .. "\n"
+  end
+
+  return text
+end
+
 vim.api.nvim_create_autocmd("CursorHold", {
   buffer = bufnr,
   callback = function()
@@ -10,5 +29,17 @@ vim.api.nvim_create_autocmd("CursorHold", {
       scope = "cursor",
     }
     vim.diagnostic.open_float(nil, opts)
+    return 
+    local diags = get_diag_at_cursor()
+    if diags == "" then
+      return
+    end
+    vim.notify(diags, "info", {
+      id = "diag",
+      title = "Diag",
+      opts = function(notif)
+        notif.timeout = 5000
+      end,
+    })
   end,
 })
