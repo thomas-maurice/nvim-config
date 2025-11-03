65caa52ef48654c0ba5b21e987c7f18e77090313
Thomas Maurice <thomas@weaviate.io>
2025-11-03 12:10:14 +0100

    Fix

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,7 @@ local function filter_lsps()
   local to_install = {}
   local lsps = require("configs.lspconfig").lsps_list
   for _, v in ipairs(lsps) do
-    if v ~= "nixd" then
+    if v ~= "nixd" and v ~= "nil_ls" and v ~= "nixfmt" then
       table.insert(to_install, v)
     end
   end
--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -11,7 +11,6 @@ return {
       "golines",
       "gomodifytags",
       "luacheck",
-      "nixfmt",
       "stylua",
     },
   },
